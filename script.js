f2c247394a9983f20688e0bbdba12e894f6a6099
Aman Singh <amansingh81757@gmail.com>
2023-01-18 07:39:03 +0530

    updated script.js

--- script.js
+++ script.js
@@ -9,3 +9,13 @@ let canc=document.getElementsByClassName("cancel")[0];
 canc.addEventListener("click", function(){
     window.location.href = "adopt.html";
 });
+
+let donatebtn=document.getElementsByClassName("donate")[0];
+donatebtn.addEventListener("click",function(){
+    alert("Thank you for donating!!");
+})
+
+let adpt=document.getElementsByClassName("submit")[0];
+adpt.addEventListener("click",function(){
+    alert("Thank you for choosing to adopt a pet. Our team will contact you for further procedure.")
+})
